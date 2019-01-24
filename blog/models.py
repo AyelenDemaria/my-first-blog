6c60b10d17b961efc46aa5cb33c3af4b31af9871
AyelenDemaria <ayedemaria@gmail.com>
2019-01-24 19:56:44 -0300

    Agregue imagen y estilos en los posts

--- blog/models.py
+++ blog/models.py
@@ -6,6 +6,8 @@ class Post(models.Model):
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
+    image = models.ImageField(upload_to='albums/images/',
+            blank=True, null=True)
     created_date = models.DateTimeField(
             default=timezone.now)
     published_date = models.DateTimeField(
